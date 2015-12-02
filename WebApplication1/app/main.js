43a87d1105828695efef9bfd0fa1981c5d4c9bbc
zvonko123 <khuuen@gmail.com>
2015-12-02 14:50:44 +0100

    ïts a kind of magic

--- WebApplication1/app/main.js
+++ WebApplication1/app/main.js
@@ -9,13 +9,19 @@
         var vm = this;
         var lista = {};
         vm.cttrVar = null;
-        vm.kita = function () {
-            vm.cttrVar=='plzs' ? console.log('hasha je servirana: '+(vm.cttrVar)) : window.alert('sayplz');
+        vm.kungfu = function () {
+            vm.cttrVar === 'plzs' ? console.log('hasha je servirana: ' + 'v=0p_1QSUsbsM') : window.alert('sayplz');
             
+        }
+
+        vm.imHungry = function () {
+            vm.cttrVar === 'plzs' ? console.log('hasha je servirana: ' + (vm.cttrVar)) : window.alert('sayplz');
         }
 
         //vm.kita = lista.a;
         //put kasha here
+
+        
         
     }
 
